d1ab1fa858a437ff4c7bb99ba9f057ab12c83962
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-04-20 17:37:17 +0200

    section 1

---
+++ js/myscript.js
@@ -0,0 +1,26 @@
+var vm = new Vue({
+  el: "#app",
+  data: {
+    contacts: [{
+        name: 'Modular',
+        icon: 'fas fa-server',
+        description: 'Composed in pseudo-Latin language which more or less corresponds.'
+      },
+      {
+        name: 'Responsive',
+        icon: 'far fa-caret-square-up',
+        description: 'Composed in pseudo-Latin language which more or less corresponds.'
+      },
+      {
+        name: 'Customizable',
+        icon: 'far fa-plus-square',
+        description: 'Composed in pseudo-Latin language which more or less corresponds.'
+      },
+      {
+        name: 'Scalable',
+        icon: 'fas fa-expand-arrows-alt',
+        description: 'Composed in pseudo-Latin language which more or less corresponds.'
+      },
+    ]
+  },
+});
